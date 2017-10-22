2bd480cad39137b3f66bbf324060b653f2778a38
lightalkmst <rook_master2000@yahoo.com>
2017-10-22 00:56:57 -0400

    update

--- app.js
+++ app.js
@@ -1,156 +1,166 @@
-// Page design:
-//  Invisible element to prompt impaired users to switch to accessibility mode
-//  Function panel
-//   Add Upright button
-//    Turns click mode into setting cards
-//    Disabled if thrown
-//   Add Rotated button
-//    Turns click mode into setting rotated cards
-//    Disabled if thrown
-//   Clear button
-//    Removes all placeholders
-//   Clear Spread
-//    Removes all cards
-//    Sets state to neutral
-//   Shuffle
-//    Shuffles deck
-//    Disabled if thrown
-//   Throw button
-//    Deals cards
-//    Sets state to thrown
-//    Disabled if thrown or no cards added
-//   Clipboard button
-//    Saves spread/throw URL to clipboard
-//  Card field
-//    Draws placeholders in positions cards were added if neutral
-//    Draws all cards in positions cards were added if thrown
+const s3_address = 'https://s3.amazonaws.com/modern-tarot/'
 
+const rider_waite = [
+  ... L.range (0) (21),
+  ... F.p (['W', 'S', 'C', 'P']) (
+    L.map (h => L.map (h2 => h2 + h) ([... L.range (1) (10), 'P', 'N', 'Q', 'K']))
+    >> L.reduce (L.append)
+  ),
+]
 
 var main = sources => {
   var {DOM: DOM} = sources
 
   const add_upright = '#add_upright'
-  const add_rotated = '#add_rotated'
+  const throw_cards = '#throw_cards'
   const clear_cards = '#clear_cards'
   const clear_spread = '#clear_spread'
-  const shuffle_deck = '#shuffle_deck'
-  const throw_cards = '#throw_cards'
   const clipboard = '#clipboard'
 
   const mat = '#mat'
 
-  const ctrl_panel_width = 100
-  const ctrl_panel_width_str = ctrl_panel_width + 'px'
+  const ctrl_panel_height = 35
+
+  const card_height = 260
+  const card_width = 150
 
   var get_clicks = s => DOM.select (s).events ('click')
 
-  // TODO deck selection (Rider-Waite, Thoth, Wildwood)
+  // TODO figure out how to derive initial spread if url parameter is given
+
+  var time$ = xs.periodic (10)
 
   var mode$ =
-    xs.merge (...[
-      get_clicks (add_upright).mapTo ('upright'),
-      get_clicks (add_rotated).mapTo ('rotated'),
-      get_clicks (clear_cards).mapTo (null),
-      get_clicks (clear_spread).mapTo (null),
-      get_clicks (shuffle_deck).mapTo (null),
-      get_clicks (throw_cards).mapTo (null),
+    xs.merge (... [
+      get_clicks (throw_cards).mapTo ('thrown'),
+      get_clicks (clear_cards).mapTo ('upright'),
+      get_clicks (clear_spread).mapTo ('upright'),
     ])
-
-  var thrown$ =
-    xs.merge (...[
-      get_clicks (clear_cards).mapTo (false),
-      get_clicks (clear_spread).mapTo (false),
-      get_clicks (throw_cards).mapTo (true),
-    ])
-
-  // TODO figure out how to derive initial spread if url parameter is given
-  var orientations$ =
-    xs.merge (...[
-      get_clicks (add_upright).mapTo ('upright'),
-      get_clicks (add_rotated).mapTo ('rotated'),
-    ])
-
-  var positions$ =
-    xs.combine (...[
-      mode$,
-      orientation$,
-      get_clicks (mat).map (h => [h.x, h.y]),
-    ])
-      .filter (h => h[0])
-      .map (h => [h[1], h[2]])
+      .startWith ('upright')
 
   var placements$ =
-    xs.merge (...[
+    xs.merge (... [
+      get_clicks (mat).map (h => [h.x, h.y]),
       get_clicks (clear_spread).mapTo ('cleared'),
-      positions$,
     ])
-      .fold ((a, h) => h == 'cleared' ? [] : [h, ...a], [])
+      .fold ((a, h) => h == 'cleared' ? [] : [... a, [h[0], h[1]]], [])
 
-  var cards$ =
-    xs.combine (...[
-      thrown$,
+  // TODO deck selection (Rider-Waite, Thoth, Wildwood)
+  var deck$ = []
+
+  var cards_dom$ =
+    xs.combine (... [
+      mode$,
       placements$,
     ])
-      .map (([thrown, placements]) => {
+      .map (([mode, placements]) => {
+        var shuffle = deck => {
+          var ans = []
+          var cards = F.p (deck) (
+            L.map (h => [h, true])
+            >> D.create
+          )
+          while (ans.length < deck.length) {
+            var ks = D.keys (cards)
+            var card = ks[Math.floor (Math.random () * ks.length)]
+            delete cards[card]
+            ans = L.cons (card) (ans)
+          }
+          return ans
+        }
 
+        var deck = shuffle (rider_waite) // TODO deck selection
+
+        var draw = () => {
+          if (deck.length) {
+            var ans = L.head (deck)
+            deck = L.tail (deck)
+            return ans
+          }
+          else {
+            return 'images/cards/rider-waite/back.jpg'
+          }
+        }
 
         var get_image =
-          thrown
-          ? F.const ('placeholder.jpg')
-          : (h => {
-            // TODO decide which card is rendered
+          mode == 'thrown'
+          ? (h => s3_address + 'images/cards/rider-waite/' + draw () + '.jpg')
+          : F.const (s3_address + 'images/cards/rider-waite/back.jpg')
+
+        var i = 0
+        var get_elem = h =>
+          img ({
+            attrs: {src: get_image (h)},
+            style: {
+              height: card_height + 'px',
+              width: card_width + 'px',
+              position: 'absolute',
+              top: h[1] - card_height / 2 - ctrl_panel_height + 'px',
+              left: h[0] - card_width / 2 + 'px',
+              'z-index': i++,
+            },
           })
 
-        return
+        return div (L.map (get_elem) (placements))
       })
+      .startWith (div ())
 
 
   return {
     DOM: (
-      xs.combine (...[
-        xs.of (1),
+      xs.combine (... [
         cards_dom$,
+        mode$,
+        time$,
       ])
         .map (([
-          c,
           cards_dom,
+          mode,
         ]) => {
           var buttons = [
-            button ('#add_upright', {style: {width: '100px'}}, [
-              'Add Upright',
-            ]),
-            br (),
-            button ('#add_rotated', {style: {width: '100px'}}, [
-              'Add Rotated',
-            ]),
-            br (),
-            button ('#clear_cards', {style: {width: '100px'}}, [
-              'Clear Cards',
-            ]),
-            br (),
-            button ('#clear_spread', {style: {width: '100px'}}, [
-              'Clear Spread',
-            ]),
-            br (),
-            button ('#shuffle', {style: {width: '100px'}}, [
-              'Shuffle',
-            ]),
-            br (),
-            button ('#throw', {style: {width: '100px'}}, [
-              'Throw',
-            ]),
+            button (throw_cards, {
+              style: {width: '100px'},
+              attrs: mode == 'thrown' ? {disabled: true} : undefined,
+            }, ['Throw']),
+            button (clear_cards, {style: {width: '100px'}}, ['Clear Cards']),
+            button (clear_spread, {style: {width: '100px'}}, ['Clear Spread']),
+            button (clipboard, {style: {width: '100px'}}, ['Clipboard'])
           ]
           return (
-            table ({
+            div ({
               style: {
                 height: '100%',
                 width: '100%',
               }
             }, [
-              tr ([
-                td (buttons),
-                td ('#mat', {style: {width: '100%'})
-              ]),
+              ... buttons,
+              mode == 'thrown'
+              ? (
+                div ({
+                  style: {
+                    position: 'fixed',
+                    top: ctrl_panel_height + 'px',
+                    bottom: '0px',
+                    left: '0px',
+                    right: '0px',
+                  },
+                }, [
+                  cards_dom,
+                ])
+              )
+              : (
+                div ('#mat', {
+                  style: {
+                    position: 'fixed',
+                    top: ctrl_panel_height + 'px',
+                    bottom: '0px',
+                    left: '0px',
+                    right: '0px',
+                  },
+                }, [
+                  cards_dom,
+                ])
+              )
             ])
           )
         })
