018f3286f170f7f8e9f0409172ff07051a05fb78
lightalkmst <rook_master2000@yahoo.com>
2017-10-22 23:23:18 -0400

    reversals

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
+// TODO: shrink card sizes on mobile
+
 const s3_address = 'https://s3.amazonaws.com/modern-tarot/'
 
-const rider_waite = [
+const rider_waite = [[
   ... L.range (0) (21),
   ... F.p (['W', 'S', 'C', 'P']) (
     L.map (h => L.map (h2 => h2 + h) ([... L.range (1) (10), 'P', 'N', 'Q', 'K']))
     >> L.reduce (L.append)
   ),
-]
+], 'images/cards/rider-waite/']
+
+const thoth = [[
+  ... L.range (0) (21),
+  ... F.p (['W', 'S', 'C', 'P']) (
+    L.map (h => L.map (h2 => h2 + h) ([... L.range (1) (10), 'S', 'Q', 'P', 'K']))
+    >> L.reduce (L.append)
+  ),
+], 'images/cards/thoth/']
+
+// greenwood deck
 
 var main = sources => {
   var {DOM: DOM} = sources
 
   const add_upright = '#add_upright'
   const throw_cards = '#throw_cards'
-  const clear_cards = '#clear_cards'
+  const clear_throw = '#clear_throw'
   const clear_spread = '#clear_spread'
   const clipboard = '#clipboard'
+  const reversals = '#reversals'
 
   const mat = '#mat'
 
   const ctrl_panel_height = 35
 
-  const card_height = 260
-  const card_width = 150
+  const card_height = 208
+  const card_width = 120
+
+  var serialize = () => {
+    var ans = ''
+    location.hash = ans
+  }
+
+  var deserialize = () => {}
 
   var get_clicks = s => DOM.select (s).events ('click')
 
@@ -30,30 +50,36 @@ var main = sources => {
 
   var time$ = xs.periodic (10)
 
-  var mode$ =
+  var thrown$ =
     xs.merge (... [
-      get_clicks (throw_cards).mapTo ('thrown'),
-      get_clicks (clear_cards).mapTo ('upright'),
-      get_clicks (clear_spread).mapTo ('upright'),
+      get_clicks (throw_cards).mapTo (true),
+      get_clicks (clear_throw).mapTo (false),
+      get_clicks (clear_spread).mapTo (false),
     ])
-      .startWith ('upright')
+      .startWith (false)
 
   var placements$ =
     xs.merge (... [
-      get_clicks (mat).map (h => [h.x, h.y]),
-      get_clicks (clear_spread).mapTo ('cleared'),
+      get_clicks (mat).map (h => [h.x - card_width / 2, h.y - card_height / 2]),
+      get_clicks (clear_spread).mapTo ([]),
     ])
-      .fold ((a, h) => h == 'cleared' ? [] : [... a, [h[0], h[1]]], [])
+      .fold ((a, h) => h.length ? [... a, [h[0], h[1]]] : [], [])
+
+  var reversals$ =
+    get_clicks (reversals)
+      .map (h => h.target.checked)
+      .startWith (false)
 
   // TODO deck selection (Rider-Waite, Thoth, Wildwood)
   var deck$ = []
 
   var cards_dom$ =
     xs.combine (... [
-      mode$,
+      thrown$,
       placements$,
+      reversals$,
     ])
-      .map (([mode, placements]) => {
+      .map (([thrown, placements, reversals]) => {
         var shuffle = deck => {
           var ans = []
           var cards = F.p (deck) (
@@ -69,101 +95,96 @@ var main = sources => {
           return ans
         }
 
-        var deck = shuffle (rider_waite) // TODO deck selection
+        var deck = shuffle (rider_waite[0]) // TODO deck selection
+        var path = rider_waite[1]
 
         var draw = () => {
-          if (deck.length) {
+          if (thrown && deck.length) {
+            var reversed = reversals && Math.random () < .5
             var ans = L.head (deck)
             deck = L.tail (deck)
-            return ans
+            return [ans, reversed]
           }
           else {
-            return 'images/cards/rider-waite/back.jpg'
+            return ['back', false]
           }
         }
 
-        var get_image =
-          mode == 'thrown'
-          ? (h => s3_address + 'images/cards/rider-waite/' + draw () + '.jpg')
-          : F.const (s3_address + 'images/cards/rider-waite/back.jpg')
-
-        var i = 0
-        var get_elem = h =>
-          img ({
-            attrs: {src: get_image (h)},
-            style: {
-              height: card_height + 'px',
-              width: card_width + 'px',
-              position: 'absolute',
-              top: h[1] - card_height / 2 - ctrl_panel_height + 'px',
-              left: h[0] - card_width / 2 + 'px',
-              'z-index': i++,
-            },
-          })
-
-        return div (L.map (get_elem) (placements))
+        return div (L.mapi (i => h => {
+          var card = draw ()
+          var image = `${s3_address}${path}${thrown ? card[0] : 'back'}.jpg`
+          return (
+            img (card[1] ? '.reversed' : '.upright', {
+              attrs: {src: image},
+              style: {
+                height: card_height + 'px',
+                width: card_width + 'px',
+                position: 'absolute',
+                top: h[1] - ctrl_panel_height + 'px',
+                left: h[0] + 'px',
+                'z-index': i,
+              },
+            })
+          )
+        }) (placements))
       })
       .startWith (div ())
-
 
   return {
     DOM: (
       xs.combine (... [
         cards_dom$,
-        mode$,
+        thrown$,
         time$,
       ])
         .map (([
           cards_dom,
-          mode,
-        ]) => {
-          var buttons = [
+          thrown,
+        ]) =>
+          div ({
+            style: {
+              height: '100%',
+              width: '100%',
+            }
+          }, [
+            // control bar
             button (throw_cards, {
               style: {width: '100px'},
-              attrs: mode == 'thrown' ? {disabled: true} : undefined,
+              attrs: thrown ? {disabled: true} : undefined,
             }, ['Throw']),
-            button (clear_cards, {style: {width: '100px'}}, ['Clear Cards']),
+            button (clear_throw, {style: {width: '100px'}}, ['Clear Cards']),
             button (clear_spread, {style: {width: '100px'}}, ['Clear Spread']),
-            button (clipboard, {style: {width: '100px'}}, ['Clipboard'])
-          ]
-          return (
-            div ({
-              style: {
-                height: '100%',
-                width: '100%',
-              }
-            }, [
-              ... buttons,
-              mode == 'thrown'
-              ? (
-                div ({
-                  style: {
-                    position: 'fixed',
-                    top: ctrl_panel_height + 'px',
-                    bottom: '0px',
-                    left: '0px',
-                    right: '0px',
-                  },
-                }, [
-                  cards_dom,
-                ])
-              )
-              : (
-                div ('#mat', {
-                  style: {
-                    position: 'fixed',
-                    top: ctrl_panel_height + 'px',
-                    bottom: '0px',
-                    left: '0px',
-                    right: '0px',
-                  },
-                }, [
-                  cards_dom,
-                ])
-              )
-            ])
-          )
-        })
+            input (reversals, {attrs: D.extend ({type: 'checkbox'}) (thrown ? {disabled: true} : {})}), 'Reversals',
+            // the card mat; the switch is to disable adding cards while thrown
+            thrown
+            ? (
+              div ({
+                style: {
+                  position: 'fixed',
+                  top: ctrl_panel_height + 'px',
+                  bottom: '0px',
+                  left: '0px',
+                  right: '0px',
+                },
+              }, [
+                cards_dom,
+              ])
+            )
+            : (
+              div ('#mat', {
+                style: {
+                  position: 'fixed',
+                  top: ctrl_panel_height + 'px',
+                  bottom: '0px',
+                  left: '0px',
+                  right: '0px',
+                },
+              }, [
+                cards_dom,
+              ])
+            )
+          ])
+        )
     ),
   }
 }
